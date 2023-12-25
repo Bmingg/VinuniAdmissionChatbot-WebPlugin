e08d46ec51d0bbce3810848a2f80cef07a5f1e4d
Vu Binh Minh <21minh.vb@vinuni.edu.vn>
2023-12-25 11:08:06 +0700

    Add ip retrieval

--- script.js
+++ script.js
@@ -131,3 +131,18 @@ function openIntentList() {
         intents[i].style.display = "block";
   }
 }
+
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+var response = "";
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  console.log(ip);
+  response = ip;
+});
+
+// jQuery.get
