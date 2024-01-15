ca6e95aca41aa0ad4c92904aa31d6439137dbe0d
Vu Binh Minh <21minh.vb@vinuni.edu.vn>
2024-01-15 14:24:17 +0700

    Code clean-up

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var endChat = false;
 var userHash = "";
 var topic = "";
 var counter = 0;
-var input_disabled = true;
 const API_ENDPOINT = "https://us-central1-vingenie.cloudfunctions.net/Vingenie2"
 const BOT_NAME = "BOT";
 const PERSON_NAME = "VinNole";
@@ -107,9 +106,8 @@ async function refreshChat() {
   input_send_btn.style.cursor = "not-allowed";
   input.style.cursor = "not-allowed";
   loader.style.display = "none";
-  input_send_btn.disabled = false;
+  input_send_btn.disabled = true;
   input.disabled = true;
-  input_disabled = true;
   input.placeholder = "Please choose an intent first!";
   const messages = document.querySelectorAll('.left-msg, .right-msg');
   messages.forEach(message => {
@@ -274,7 +272,6 @@ function updateQuestionText(intentName) {
     input_send_btn.disabled = false;
     input.disabled = false;
     
-    input_disabled = false;
     input.placeholder = "Ask me anything..."
   }
   const questionTextElement = document.querySelector('#default-text');
@@ -282,13 +279,6 @@ function updateQuestionText(intentName) {
   if (questionTextElement) {
     questionTextElement.textContent = `What do you want to ask about ${intentName}?`;
     messageBubble.style.display = 'block';
-  }
-}
-
-function clickMessage() {
-  input_send_btn = document.getElementsByClassName("msger-send-btn")[0];
-  if (input_disabled) {
-    input_send_btn.disabled = true;
   }
 }
 
